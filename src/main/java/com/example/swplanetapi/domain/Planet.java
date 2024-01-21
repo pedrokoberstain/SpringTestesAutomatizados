3c452099f76d98f0db98152709a9c444fe22ae45
Kober <pedrokoberstain2@gmail.com>
2024-01-21 09:22:28 -0300

    Adicionando novos testes para criação de um planet já existente

--- src/main/java/com/example/swplanetapi/domain/Planet.java
+++ src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -1,10 +1,7 @@
 package com.example.swplanetapi.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
@@ -14,9 +11,14 @@ public class Planet {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
+@NotEmpty
+@Column(nullable = false, unique = true)
   private String name;
+  @NotEmpty
+  @Column(nullable = false)
   private String climate;
+  @NotEmpty
+  @Column(nullable = false)
   private String terrain;
 
   public Planet() {
---
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.example.swplanetapi.domain;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ThrowsException() {
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        // TODO implement
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+        // TODO implement
+    }
+}
